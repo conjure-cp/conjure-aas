da5366f4c3de29b67be03e68ca49aadd921fa613
Özgür Akgün <ozgurakgun@gmail.com>
2023-01-26 21:44:30 +0000

    introducing a model-cache

--- conjure-aas.js
+++ conjure-aas.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs');
 const { spawn } = require('node:child_process');
+const md5 = require('md5');
 
 let config = fs.readFileSync("config.json");
 config = JSON.parse(config);
@@ -12,19 +13,46 @@ const app = express();
 app.use(express.json())
 app.use(express.static('static'))
 
+fs.mkdirSync('model-cache', { recursive: true });
+fs.mkdirSync('conjure-output', { recursive: true });
+let logStream = fs.createWriteStream('logs.txt', { flags: 'a' });
+
+function log(message) {
+    let timestamp = new Date().toISOString();
+    logStream.write(`${timestamp} ${message}\n`);
+    console.log(`${timestamp} ${message}`);
+}
 
 function submitHandler(req, res) {
-    let thisJobId = config.nextJobId++;
-    fs.writeFileSync("config.json", JSON.stringify(config));
 
-    console.log(`received ${thisJobId}`)
-    // console.log(req.body);
+    // next job id
+    let thisJobId = config.nextJobId++;
+    fs.writeFileSync('config.json', JSON.stringify(config));
+
+    log(`submit ${thisJobId}`)
 
     // create directory
-    fs.mkdirSync(`conjure-output/${thisJobId}`, { recursive: true });
+    fs.mkdirSync(`conjure-output/${thisJobId}`);
+
+    // we cache the essence + eprime in a model-cache to avoid rerunning conjure-modelling
+    let cacheKey = md5(req.body.model);
+    let cacheHit = false;
 
     // create Conjure's input files
-    fs.writeFileSync(`conjure-output/${thisJobId}/model.essence`, req.body.model);
+    try {
+        // can we copy from the cache?
+        fs.copyFileSync(`model-cache/${cacheKey}.conjure-checksum`, `conjure-output/${thisJobId}/.conjure-checksum`);
+        fs.copyFileSync(`model-cache/${cacheKey}.essence`, `conjure-output/${thisJobId}/model.essence`);
+        fs.copyFileSync(`model-cache/${cacheKey}.eprime`, `conjure-output/${thisJobId}/model000001.eprime`);
+        log(`submit ${thisJobId} - cache hit`);
+        cacheHit = true;
+    } catch (e) {
+        log(`submit ${thisJobId} - cache miss`);
+        fs.writeFileSync(`conjure-output/${thisJobId}/model.essence`, req.body.model);
+        cacheHit = false;
+    }
+
+    // the data file
     fs.writeFileSync(`conjure-output/${thisJobId}/data.json`, req.body.data);
 
     // the user can specify which solver to use, default is kissat
@@ -34,44 +62,44 @@ function submitHandler(req, res) {
     }
 
     // run conjure
-    conjure_spawn = spawn('conjure',
+    let conjure_spawn = spawn('conjure',
         ['solve'
             , `conjure-output/${thisJobId}/model.essence`
             , `conjure-output/${thisJobId}/data.json`
             , '--output-directory', `conjure-output/${thisJobId}`
             , '--solver', solver
-            , '--output-format', 'json'
-            // , '--copy-solutions', 'no'
+            , '--output-format=json'
+            , '--copy-solutions=no'
         ]);
 
-    conjure_spawn.stdout.on('data', (data) => {
-        console.log(`stdout (${thisJobId}): ${data}`);
-    });
-
-    conjure_spawn.stderr.on('data', (data) => {
-        console.error(`stderr (${thisJobId}): ${data}`);
-    });
-
+    let thisLogStream = fs.createWriteStream(`conjure-output/${thisJobId}/logs.txt`, { flags: 'a' });
+    conjure_spawn.stdout.pipe(thisLogStream);
+    conjure_spawn.stderr.pipe(thisLogStream);
     conjure_spawn.on('close', (code) => {
-        if (code != 0) {
-            console.log(`child process (${thisJobId}) exited with code ${code}`);
+        if (code == 0 && cacheHit == false) {
+            // save the model in the model-cache
+            fs.copyFileSync(`conjure-output/${thisJobId}/.conjure-checksum`, `model-cache/${cacheKey}.conjure-checksum`);
+            fs.copyFileSync(`conjure-output/${thisJobId}/model.essence`, `model-cache/${cacheKey}.essence`);
+            fs.copyFileSync(`conjure-output/${thisJobId}/model000001.eprime`, `model-cache/${cacheKey}.eprime`);
+            log(`submit ${thisJobId} - cache populated ${cacheKey}`);
         }
+        log(`submit ${thisJobId} - exitcode ${code}`);
+        thisLogStream.write(`submit ${thisJobId} - exitcode ${code}\n`);
     });
 
-    console.log(`spawned ${thisJobId}`)
+    log(`submit ${thisJobId} - spawned`)
     res.json({ jobid: thisJobId });
 }
 
 function getHandler(req, res) {
     let jobid = req.body.jobid;
-    console.log(`get ${jobid}`);
     const solutionFile = `conjure-output/${jobid}/model000001-data-solution000001.solution.json`
     if (fs.existsSync(solutionFile)) {
-        console.log(`get ${jobid}: ok`);
+        log(`get ${jobid} - ok`);
         res.json({ status: "ok", solution: JSON.parse(fs.readFileSync(solutionFile)) });
     }
     else {
-        console.log(`get ${jobid}: wait`);
+        log(`get ${jobid} - wait`);
         res.json({ status: "wait" });
     }
 }
@@ -80,5 +108,3 @@ app.post('/submit', submitHandler);
 app.post('/get', getHandler);
 
 app.listen(8080, () => console.log('listening'));
-
-// fs.writeFileSync("config.json", JSON.stringify(config));
