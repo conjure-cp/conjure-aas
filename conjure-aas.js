c8342885bc5ff502f75f42e8e71a75c8d9b88428
Özgür Akgün <ozgurakgun@gmail.com>
2026-07-10 15:09:49 +0100

    feat: raise JSON body limit to 10mb

--- conjure-aas.js
+++ conjure-aas.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
 app.use(express.static("static"));
 
 fs.mkdirSync("model-cache", { recursive: true });
